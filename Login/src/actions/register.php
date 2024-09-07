7155af9b26e1fe7309eb5e54bbba1edfb2e68382
Hagar Elbakry <161838415+Hagar-Elbakry@users.noreply.github.com>
2024-09-07 20:14:35 +0300

    Update register.php
    Added password hashing functionality before storing user data in the database to enhance security

--- Login/src/actions/register.php
+++ Login/src/actions/register.php
@@ -10,9 +10,15 @@ if($_SERVER["REQUEST_METHOD"]=="POST") {
         require_once "dbh.php";
         $sql = "INSERT INTO users (firstname, lastname, pwd, email) VALUES (:firstname, :lastname, :pwd, :email)";
         $stmt=$pdo->prepare($sql);
+
+        $options = [
+            'cost' => 12
+        ];
+        $pwdHashed = password_hash($pwd, PASSWORD_BCRYPT, $options);
+
         $stmt->bindParam(":firstname", $firstName);
         $stmt->bindParam(":lastname", $lastName);
-        $stmt->bindParam(":pwd", $pwd);
+        $stmt->bindParam(":pwd", $pwdHashed);
         $stmt->bindParam(":email", $email);
         $stmt->execute();
         $pdo = null;
